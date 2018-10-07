1a7e0e4a9da5352d4405bb5f493498965a604f29
prkala <prkala@PRKALA-IN.oradev.oraclecorp.com>
2018-10-08 00:38:23 +0530

    Modularizing everything. Working okay.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,35 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent,
+    ProjectComponent,
+    SubscribeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'projects', component: ProjectComponent },
+      { path: 'subscribe', component: SubscribeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
